f7f164a28a936ba1af5bbdcfb4a1936046951388
Centrix <fagfagas39@gmail.com>
2019-11-11 23:13:35 -0800

    Update sv_warden.lua

--- lua/autorun/server/sv_warden.lua
+++ lua/autorun/server/sv_warden.lua
@@ -116,13 +116,13 @@ end
 						The return value from the IP check. False if connection failed.
 
 				POSSIBLE VALUES:
-					Given an IP address, the system will return a probabilistic value (between a value of 0 and 1) 
-					of how likely the IP is a VPN / proxy / hosting / bad IP. 
-					A value of 1 means that IP is explicitly banned (a web host, VPN, or TOR node) by their dynamic lists. 
+					Given an IP address, the system will return a probabilistic value (between a value of 0 and 1)
+					of how likely the IP is a VPN / proxy / hosting / bad IP.
+					A value of 1 means that IP is explicitly banned (a web host, VPN, or TOR node) by their dynamic lists.
 					Otherwise, the output will return a real number value between 0 and 1, of how likely
-					the IP is bad / VPN / proxy, which is inferred through machine learning & probability theory 
-					techniques using dynamic checks with large datasets. Billions of new records are parsed 
-					each month to ensure the datasets have the latest information and old records automatically expire. 
+					the IP is bad / VPN / proxy, which is inferred through machine learning & probability theory
+					techniques using dynamic checks with large datasets. Billions of new records are parsed
+					each month to ensure the datasets have the latest information and old records automatically expire.
 					The system is designed to be efficient, fast, simple, and accurate.
 
 		[boolean] useCache = true
